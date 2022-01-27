1cf5233cf6735b31501aa10b3ede4300031ff251
nasif <nasifshahriar4@gmail.com>
2022-01-28 00:33:43 +0600

    1st commit

--- Loops.py
+++ Loops.py
@@ -1,3 +1,7 @@
 if __name__ == '__main__':
     n = int(input())
+    #1st solution
     [print(i**2) for i in range(n)]
+    #2nd solution
+    print(*[num**2 for num in range(n)],sep='\n')
+    ## '*' used for unpacking list items
